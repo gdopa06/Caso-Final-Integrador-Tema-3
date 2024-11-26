1fcddcd3100cebb0032a8a035a578c9325852a75
Gonzalo Müller Fernández <gmullfer@myuax.com>
2024-11-26 11:41:31 +0100

    finalizacion del proyecto

---
+++ Funciones/ejecutar.h
@@ -0,0 +1,11 @@
+//
+// Created by gonza on 26/11/2024.
+//
+
+#ifndef EJECUTAR_H
+#define EJECUTAR_H
+
+void load_script(const char* filename, bool show_script);
+void load_script();
+
+#endif //EJECUTAR_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Funciones/ejecutar.h"
+//
+// Created by gonza on 26/11/2024.
+//
+int main() {
+
+    load_script();
+    return 0;
+}
